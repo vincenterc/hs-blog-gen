cc698e24cf5065c1d0bfb86bc197a4e9704a8e74
vincenter <vincenterr@yahoo.com.tw>
2024-11-07 15:25:12 +0800

    Add ul_, ol_ and code_ in the Html.Internal module

--- Html/Internal.hs
+++ Html/Internal.hs
@@ -25,6 +25,17 @@ p_ = Structure . el "p" . escape
 h1_ :: String -> Structure
 h1_ = Structure . el "h1" . escape
 
+ul_ :: [Structure] -> Structure
+ul_ =
+  Structure . el "ul" . concat . map (el "li" . getStructureString)
+
+ol_ :: [Structure] -> Structure
+ol_ =
+  Structure . el "ol" . concat . map (el "li" . getStructureString)
+
+code_ :: String -> Structure
+code_ = Structure . el "pre" . escape
+
 append_ :: Structure -> Structure -> Structure
 append_ c1 c2 =
   Structure (getStructureString c1 <> getStructureString c2)
