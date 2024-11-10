8670cdfceb80d8efe7080625da7b0218846dfcd7
vincenter <vincenterr@yahoo.com.tw>
2024-11-10 23:56:49 +0800

    Use the library, optparse-applicative, to create a fancier command-line interface

--- src/HsBlog.hs
+++ src/HsBlog.hs
@@ -1,5 +1,6 @@
 module HsBlog
-  ( main,
+  ( convertSingle,
+    convertDirectory,
     process,
   )
 where
@@ -7,43 +8,22 @@ where
 import HsBlog.Convert (convert)
 import qualified HsBlog.Html as Html
 import qualified HsBlog.Markup as Markup
-import System.Directory (doesFileExist)
-import System.Environment (getArgs)
+import System.IO
 
-main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    [] -> do
-      content <- getContents
-      putStrLn (process "Empty title" content)
-    [input, output] -> do
-      content <- readFile input
-      exists <- doesFileExist output
-      let writeResult = writeFile output (process input content)
-      if exists
-        then whenIO confirm writeResult
-        else writeResult
-    _ ->
-      putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
+convertSingle :: Html.Title -> Handle -> Handle -> IO ()
+convertSingle title input output = do
+  content <- hGetContents input
+  hPutStrLn output (process title content)
+
+convertDirectory :: FilePath -> FilePath -> IO ()
+convertDirectory = error "Not implemented"
 
 process :: Html.Title -> String -> String
 process title = Html.render . convert title . Markup.parse
 
-confirm :: IO Bool
-confirm = do
-  putStrLn "Are you sure? (y/n)"
-  answer <- getLine
-  case answer of
-    "y" -> pure True
-    "n" -> pure False
-    _ -> do
-      putStrLn "Invalid response. use y or n"
-      confirm
-
-whenIO :: IO Bool -> IO () -> IO ()
-whenIO cond action = do
-  result <- cond
-  if result
-    then action
-    else pure ()
+-- whenIO :: IO Bool -> IO () -> IO ()
+-- whenIO cond action = do
+--   result <- cond
+--   if result
+--     then action
+--     else pure ()
